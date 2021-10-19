09bd95cbd3a5d357c1d95d443a8384c1c9cca4b5
VicShm <vicshm@gmail.com>
2021-10-19 21:06:46 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,38 @@
+# Excel And Art
+## Image to Excel converter
+
+Some time ago I saw some works by a Japanese artist Tatsuo Horiuchi who decided to use Microsoft Excel as his drawing tool.
+
+I liked it a lot, it's true art!
+
+You can see it [here](https://pasokonga.com/).
+Beautiful, isn't it?
+
+Matsuo Horiuchi is an artist, yeah, but I'm a programmer! So how can I use Excel the same way?
+
+Let's get to work!
+* Python 3.9.5.
+* Python Libraries: xlsxwriter, PIL, time.
+* Microsoft Exel 2016, WPS Office 11.2 also work well.
+
+I wrote this script: excelandart.py
+
+Specify image file in the 7th line:
+- img_file = './cat/cat.png'
+
+In the 8th line specify the Excel file into wich we convert the image:
+- exl_file = './cat/cat.xlsx'
+
+Let's run:
+python excelandart.py
+
+And also keep in mind computer performance.
+For example my laptop (2012, Intel i5, 4 Gb RAM, SSD) converted cat.png for 57 minutes and same operation took 11 minutes on Intel i7-8700.
+
+And also Excel suddenly failed with an error of the cell format opening the workbook. I  didn't look any further and just сonverted the image from png to jpg and it worked.
+
+Examples are in catalogues cat, eye, girl.
+
+Best Regards.
+
+Victor Shm.
---
+++ excelandart.py
@@ -0,0 +1,52 @@
+﻿import xlsxwriter
+from PIL import Image
+import time
+
+start_time = time.time()
+
+img_file = './cat/cat.png'	# Image file.
+exl_file = './cat/cat.xlsx'	# Excel file.
+
+# Open image file
+img = Image.open(img_file)
+width, height = img.size
+obj = img.load()
+
+exl_book = xlsxwriter.Workbook(exl_file)	# Create Excel book
+exl_sheet = exl_book.add_worksheet()		# Create Excel sheet
+
+cell_format_list = []	# Cells format list.
+pixel_list = []		# List of all pixel colors
+
+# List of deduplicated colors.
+for x in range(0, width):
+    for y in range(0, height):
+        rgb = (obj[x, y][0], obj[x, y][1], obj[x, y][2])  # pillow gets pixel color in RGB
+        pixel_list.append(f'#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}')  # In Excel  must be HEX cells color
+unique_colors = list(set(pixel_list))
+
+# Creating Excel Cell formats list.
+for color in unique_colors:
+    cell_format = exl_book.add_format()
+    cell_format.set_pattern(1)
+    cell_format.set_bg_color(color.strip())
+    cell_format_list.append(cell_format)
+
+# Consistently read the color of each pixel and specify the color of the corresponding cells in Excel.
+for x in range(0, width):
+    for y in range(0, height):
+        rgb = (obj[x, y][0], obj[x, y][1], obj[x, y][2])
+        hex_pixel_color = f'#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}'
+        items = [item for item in cell_format_list if item.bg_color == hex_pixel_color]
+        exl_sheet.write(y, x, '', items[0])
+    print(width-x)
+
+# Specify the required cell size and page scale.
+print("Few seconds more...")
+exl_sheet.set_default_row(5)
+exl_sheet.set_column(0, width, 0.5)
+exl_sheet.zoom = 20
+exl_sheet
+exl_book.close()
+print("Done!")
+print(time.time() - start_time)
